ce6cd952ed99fcd784ab6a76179cb9c5a6e67e7d
deansmith26 <132027700+deansmith26@users.noreply.github.com>
2024-07-03 15:29:11 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Hello World'
+copyright = '2024, DSm'
+author = 'DSm'
 
 release = '0.1'
 version = '0.1.0'
